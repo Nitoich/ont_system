dc8da0f10a3863355ff0011067ab7752b1cf8f58
Nitoich <bcmamka@gmail.com>
2022-03-24 12:33:17 +0500

    Подготовил форму для авторизация

--- resources/views/templates/forms/login.blade.php
+++ resources/views/templates/forms/login.blade.php
@@ -1,9 +1,10 @@
 <div class="form login">
     <div class="wrapper">
-        <form class="form-content">
+        <form action="/login" method="post" class="form-content">
+            @csrf
             <input type="username" placeholder="Логин">
             <input type="password" placeholder="Пароль">
-            <button id="auth-button">Войти</button>
+            <button id="auth-button" type="submit">Войти</button>
         </form>
     </div>
 </div>
