f9fb1e9578edf1cae2472ed326ca9cdccf228c04
Nitoich <bcmamka@gmail.com>
2022-03-27 09:32:33 +0300

    Добавил новый вкладки

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -19,9 +19,16 @@
             <ul class="menu__list">
                 <li class="menu__item" @click="goToSite('/')">
                     <div class="item__img">
-                        <i class="fa-solid fa-house-user"></i>
+                        <i class="fa-brands fa-firefox-browser"></i>
                     </div>
                     <div class="item__name">На сайт</div>
+                </li>
+
+                <li class="menu__item" @click="goToSite('/admin?tab=home')">
+                    <div class="item__img">
+                        <i class="fa-solid fa-house-user"></i>
+                    </div>
+                    <div class="item__name">Главная</div>
                 </li>
 
                 <li class="menu__item" @click="goToSite('/admin?tab=teachers')">
@@ -29,6 +36,27 @@
                         <i class="fa-solid fa-user"></i>
                     </div>
                     <div class="item__name">Учителя</div>
+                </li>
+
+                <li class="menu__item" @click="goToSite('/admin?tab=groups')">
+                    <div class="item__img">
+                        <i class="fa-solid fa-users"></i>
+                    </div>
+                    <div class="item__name">Группы</div>
+                </li>
+
+                <li class="menu__item" @click="goToSite('/admin?tab=rasp')">
+                    <div class="item__img">
+                        <i class="fa-solid fa-table-cells"></i>
+                    </div>
+                    <div class="item__name">Расписание</div>
+                </li>
+
+                <li class="menu__item" @click="goToSite('/admin?tab=izm')">
+                    <div class="item__img">
+                        <i class="fa-solid fa-newspaper"></i>
+                    </div>
+                    <div class="item__name">Изменения</div>
                 </li>
             </ul>
         </div>
