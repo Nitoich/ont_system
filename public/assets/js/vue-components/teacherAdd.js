35c10da5e62228b93de9645bd600fb91151148bb
Nitoich <bcmamka@gmail.com>
2022-04-04 15:59:04 +0500

    Сделал запрос на создание нового препода

--- public/assets/js/vue-components/teacherAdd.js
+++ public/assets/js/vue-components/teacherAdd.js
@@ -30,7 +30,20 @@ const TeacherAdd = {
     },
     methods: {
         addTeacher() {
-            //
+            fetch(`/admin/teacher?_token=${document.querySelector('input[name="_token"]').value}&last_name=${this.last_name}&first_name=${this.first_name}&patronymic=${this.patronymic}&login=${this.login}&password=${this.password}`, {
+                method: "post",
+                credentials: "same-origin",
+            })
+                .then(res => {
+                    if (res.status != 200) {
+                        this.error = true;
+                        this.errorText = res.json();
+                        console.log(this.errorText);
+                    } else {
+                        console.log('Succes!');
+                        window.location.reload();
+                    }
+                })
         },
         canCreateStat() {
             if (this.login == '' || this.password == '' || this.last_name == '' || this.first_name == '' || this.patronymic == '') {
